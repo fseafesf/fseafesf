a5ce8d350d6bec3a88c31ef90a41209958429fda
fseafesf <106012426+fseafesf@users.noreply.github.com>
2022-08-27 17:58:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -322,6 +322,6 @@ if __name__ == "__main__":
     chp = get_tianhang()
     # 公众号推送消息
     for user in users:
-        send_message(user, accessToken, region, weather, temp, wind_dir,aware_day, note_ch, note_en, max_temp, min_temp, sunrise,
+        send_message(user, accessToken, region, weather, temp, wind_dir,aware_days, note_ch, note_en, max_temp, min_temp, sunrise,
                      sunset, category, pm2p5, proposal, chp)
     os.system("pause")
